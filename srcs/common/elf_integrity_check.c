e4ba9bfc4ad07d811e2c8843506e28198c4a2852
Benjamin Sellem <besellem@student.42.fr>
2022-05-04 23:20:02 +0000

    boundaries better checked

--- srcs/common/elf_integrity_check.c
+++ srcs/common/elf_integrity_check.c
@@ -6,7 +6,7 @@
 /*   By: besellem <besellem@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/04 17:28:45 by besellem          #+#    #+#             */
-/*   Updated: 2022/05/04 22:21:45 by besellem         ###   ########.fr       */
+/*   Updated: 2022/05/04 23:17:57 by besellem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,9 @@
 static int	_elf32_integrity_check(const t_file *file)
 {
 	const Elf32_Ehdr	*hdr = file->p;
-	const Elf32_Shdr	*shdr = file->p + hdr->e_shoff;
+	const size_t		min_size = sizeof(*hdr) + \
+									(hdr->e_phentsize * hdr->e_phnum) + \
+									(hdr->e_shnum * hdr->e_shentsize);
 
 	// file too small to be valid
 	if (file->st.st_size < (long)sizeof(*hdr))
@@ -27,23 +29,24 @@ static int	_elf32_integrity_check(const t_file *file)
 	if (ELFCLASSNONE == hdr->e_ident[EI_CLASS])
 		return 1;
 
-	if ((size_t)file->st.st_size < hdr->e_phoff)
-		return 1;
-
-	if ((size_t)file->st.st_size < hdr->e_shoff)
+	// check boundaries
+	if ((size_t)file->st.st_size < hdr->e_phoff ||
+		(size_t)file->st.st_size < hdr->e_shoff)
 		return 1;
 
 	// file too small for supposed data
-	if ((hdr->e_shnum * sizeof(*shdr)) > (size_t)file->st.st_size)
+	if ((size_t)file->st.st_size < min_size)
 		return 1;
-	
+
 	return 0;
 }
 
 static int	_elf64_integrity_check(const t_file *file)
 {
 	const Elf64_Ehdr	*hdr = file->p;
-	const Elf64_Shdr	*shdr = file->p + hdr->e_shoff;
+	const size_t		min_size = sizeof(*hdr) + \
+									(hdr->e_phentsize * hdr->e_phnum) + \
+									(hdr->e_shnum * hdr->e_shentsize);
 
 	// file too small to be valid
 	if (file->st.st_size < (long)sizeof(*hdr))
@@ -55,16 +58,15 @@ static int	_elf64_integrity_check(const t_file *file)
 	if (ELFCLASSNONE == hdr->e_ident[EI_CLASS])
 		return 1;
 
-	if ((size_t)file->st.st_size < hdr->e_phoff)
-		return 1;
-
-	if ((size_t)file->st.st_size < hdr->e_shoff)
+	// check boundaries
+	if ((size_t)file->st.st_size < hdr->e_phoff ||
+		(size_t)file->st.st_size < hdr->e_shoff)
 		return 1;
 
 	// file too small for supposed data
-	if ((hdr->e_shnum * sizeof(*shdr)) > (size_t)file->st.st_size)
+	if ((size_t)file->st.st_size < min_size)
 		return 1;
-	
+
 	return 0;
 }
 
